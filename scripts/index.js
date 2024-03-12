9637aa4071260ba48aa4471b34b3d1f5a552f370
Nandha Kumar <nandhakumar.kumaran@cdw.com>
2024-03-12 23:17:46 +0530

    Feature Rear view mirror - day 2 challenge done

--- scripts/index.js
+++ scripts/index.js
@@ -25,11 +25,50 @@ function steerWheel(direction, isReleased) {
     console.log(steerWheelAngle+"   "+$(".steer-wheel").css("transform"));
 }
 
+function showRearViewCamera(){
+    const $video = $('#myVidPlayer');
+    let $rearViewMirror = $('.rear-view-mirror');
+    let $camera = $('.camera');
+    let w, h;
+    if($video[0].paused)
+        onCamera();
+    else    
+        offCamera();
+
+    function onCamera(){
+        window.navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then(stream => {
+            $video[0].srcObject = stream;
+            $video[0].onloadedmetadata = (e) => {
+                $video[0].play();   
+                w = $video[0].videoWidth;
+                h = $video[0].videoHeight
+            };
+            $rearViewMirror.removeClass('d-none');
+            $camera.addClass('enabled');
+        })
+        .catch(error => {
+            $rearViewMirror.addClass('d-none');
+            alert('You have to enable the mike and the camera');
+        });
+    }
+
+    function offCamera(){
+        $rearViewMirror.addClass('d-none');
+        $camera.removeClass('enabled');
+        const mediaStream = $video[0].srcObject;
+        const tracks = mediaStream.getTracks();
+        tracks.forEach(track => track.stop());
+        $video[0].pause();
+    }
+}
+
 (function(){
     $(document).on("keydown", function (event) {
         if(event.key == "ArrowRight") steerWheel("right", false);
         else if(event.key == "ArrowLeft") steerWheel("left", false);
         else if(event.key == "b" || event.key == "B") brakeActions("apply");
+        else if(event.key == "c" || event.key == "C") showRearViewCamera();
     });
 
     $(document).on("keyup", function (event) {
@@ -37,5 +76,4 @@ function steerWheel(direction, isReleased) {
         else if(event.key == "ArrowLeft") steerWheel("right", true);
         else if(event.key == "b" || event.key == "B") brakeActions("release");
     });
-
 }());
